70696987811c624ab2ad90053dcf489c03c00218
max demian <mikar@gmx.de>
2014-05-19 18:37:28 +0200

    fixing setup.py

--- setup.py
+++ setup.py
@@ -24,16 +24,15 @@ setup(
     url="https://github.com/mikar/{}".format(_name),
     author="Max Demian",
     author_email="mikar@gmx.de",
-    packages=[_name, _name + "/data"],
-    package_data={_name: ["icon.png", "history.txt", "demimove.ini", "gui.ui"]},
+    packages=[_name],
+    package_data={_name + "/data": ["icon.png", "history.txt", "demimove.ini", "gui.ui"]},
     install_package_data=True,
     entry_points={
                   "console_scripts": [
-                      "{0} = {0}.{0}:main".format(_name),
-                      "{0}-dbus = {0}.{0}dbus:main".format(_name),
+                      "{0} = {0}.cli:main".format(_name),
                   ],
                   "gui_scripts": [
-                      "{0}-ui = {0}.{0}ui:main".format(_name),
+                      "{0}-ui = {0}.gui:main".format(_name),
                   ],
               }
 )
